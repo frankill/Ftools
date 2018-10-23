71fede9979301db7c951599cdce0b06af8caa312
frankill <34159384+frankill@users.noreply.github.com>
2018-10-23 17:19:35 +0800

    Update Ftools.jl

--- src/Ftools.jl
+++ src/Ftools.jl
@@ -4,6 +4,70 @@ module Ftools
 
 	export @stack, @queue, List 
 
+	mutable struct List 
+		   first::Any
+		   second::List
+		   List() = (x = new(nothing); x.second = x )
+		   List(first, second::List) = new(first,second)
+	       end
+
+	function Base.show( io::IO, x::List)
+		x.first == nothing  && return 
+		print(io, x.first, ",")
+		show(io, x.second)
+	end 
+
+	function lextra(l::List, r::Array)
+		if l.first == nothing 
+			return r
+		else 
+			lextra(l.second, push!(r, l.first))
+		end 
+	end 
+
+	function Base.values(l::List)
+		res = [] 
+		lextra(l, res)
+	end 
+
+	function llength(l::List, r::Int=0)
+		if l.first == nothing 
+			return r
+		else 
+			llength(l.second, r+1)
+		end 
+	end 
+
+	Base.keys( l::List ) = fieldnames( typeof(l) )
+	Base.length(l::List) = llength(l)
+	Base.pop!(l::List) = (l.first,l.second) 
+	Base.copy(l::List) = List(l.first, l.second)
+
+	function Base.push!(l::List, a::Any)
+		if l.second.first == nothing
+		   l.second = List(a, List())
+		else
+			push!(l.second, a)
+		end 
+	end 
+
+	function Base.push!(a::Any, l::List )
+		tmp = List(a , List())
+		tmp.second = copy(l) 
+		l.second = tmp.second  
+		l.first = tmp.first
+	end 
+
+	function Base.push!(l::List, a::Any , test::Any)
+		if l.second.first == test
+		   tmp = List(a, l.second.second)
+		   l.second.second = tmp
+		else
+			push!(l.second, a, test)
+		end 
+	end 
+
+
 	include("Stack.jl")
 	include("Queue.jl")
 
