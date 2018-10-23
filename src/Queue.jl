10f2d1eab4126d2ff743a1603735ceaa74ede1db
frankill <34159384+frankill@users.noreply.github.com>
2018-10-23 14:52:37 +0800

    Update Queue.jl

--- src/Queue.jl
+++ src/Queue.jl
@@ -1,3 +1,12 @@
 macro queue(expr...)
-	esc(genlist(sort(collect(expr),rev=true)))
+	esc(genqueue(sort(collect(expr),rev=true)))
+end 
+
+function genqueue(e::Vector)
+	q  = Expr(:new, :list , nothing  )
+	num = length(e)
+	for i in num:-1:1
+		q = Expr(:new, :list , e[i] , q  )
+	end
+	return q 
 end 
