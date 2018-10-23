6095e13cdaef724703238dd605ed3d553a30214b
frankill <34159384+frankill@users.noreply.github.com>
2018-10-23 12:27:10 +0800

    Create Queue.jl

---
+++ src/Queue.jl
@@ -0,0 +1,42 @@
+mutable struct list 
+ 		   first
+           second::list
+           list(first) = (x = new(first); x.second = x )
+           list(first, second::list) = new(first,second)
+       end
+
+function Base.show( io::IO, x::list)
+	x.first == nothing  && return 
+	print(io, x.first, ",")
+	show(io, x.second)
+end 
+
+function lextra(l::list, r::Array)
+	if l.first == nothing 
+		return r
+	else 
+	 	lextra(l.second, push!(r, l.first))
+	end 
+end 
+
+function Base.values(l::list)
+	res = [] 
+	lextra(l, res)
+end 
+
+Base.keys( l::list ) = fieldnames( typeof(l) )
+Base.length(l::list) = length(values(l))
+ 
+macro list(expr...)
+	esc(genlist(collect(expr)))
+end 
+
+function genlist(e::Vector)
+	q  = Expr(:new, :list , e[1]  )
+
+	for i in e
+		q = Expr(:new, :list , i , q  )
+	end 
+
+	return q 
+end 
