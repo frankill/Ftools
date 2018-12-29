60ae2be01f4708cfb9b65db49ff16e74d8efee10
mac <mac@macs-iMac.local>
2018-12-29 15:48:15 +0800

    11

--- src/Ftools.jl
+++ src/Ftools.jl
@@ -4,8 +4,9 @@ module Ftools
 
 	export @stack, @queue, List ,list
 	export rep, unique, findfirst
+	export @set
 
-	mutable struct List 
+	mutable struct List
 		   first::Any
 		   second::List
 		   List() = (x = new(nothing); x.second = x )
@@ -14,74 +15,73 @@ module Ftools
 
 	list(value::Any) = List(value, List())
 	list() 		 = List()
-	
+
 	function Base.show( io::IO, x::List)
-		x.first == nothing  && return 
+		x.first == nothing  && return
 		print(io, x.first, ",")
 		show(io, x.second)
-	end 
+	end
 
 	function lextra(l::List, r::Array)
-		if l.first == nothing 
+		if l.first == nothing
 			return r
-		else 
+		else
 			lextra(l.second, push!(r, l.first))
-		end 
-	end 
+		end
+	end
 
 	function Base.values(l::List)
-		res = [] 
+		res = []
 		lextra(l, res)
-	end 
+	end
 
 	function llength(l::List, r::Int=0)
-		if l.first == nothing 
+		if l.first == nothing
 			return r
-		else 
+		else
 			llength(l.second, r+1)
-		end 
-	end 
+		end
+	end
 
 	Base.keys( l::List ) = fieldnames( typeof(l) )
 	Base.length(l::List) = llength(l)
 	Base.copy(l::List) = List(l.first, l.second)
 
 	function Base.pop!(l::List)
-		l.first == nothing && return nothing 
-		tmp = l.second.first == nothing ? List() : copy(l.second) 
+		l.first == nothing && return nothing
+		tmp = l.second.first == nothing ? List() : copy(l.second)
 		res = l.first
 		l.first =  tmp.first
 		l.second = tmp.second
 		res
-	end 
-		
+	end
+
 	function Base.push!(l::List, a::Any)
-		
+
 		if l.first == nothing
 		   l.first, l.second =  a, List()
 		else
 			push!(l.second, a)
-		end 
-	end 
+		end
+	end
 
 	function Base.push!(a::Any, l::List )
 		l.second = copy(l)
 		l.first = a
-	end 
+	end
 
 	function Base.push!(l::List, a::Any , test::Any)
 		if l.second.first == test
 		   l.second.second = List(a, l.second.second)
 		else
 			push!(l.second, a, test)
-		end 
-	end 
+		end
+	end
 
 
 	include("Stack.jl")
 	include("Queue.jl")
 	include("tools.jl")
-	include("word_split.jl")
 
-	
-end 
+
+end
